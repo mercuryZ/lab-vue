cdc433e697cebd77f8e8194decd7fc3d9b459b87
mercuryZ <15210820708@163.com>
2020-11-18 13:44:39 +0800

    init

---
+++ src/components/Lab.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div>
+
+    <p>Hello World</p>
+
+  </div>
+
+</template>
+
+<script>
+  export default{
+    name: 'LabData',
+    data(){
+
+      return {}
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
